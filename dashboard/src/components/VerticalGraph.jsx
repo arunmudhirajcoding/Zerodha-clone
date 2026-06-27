e5f467665783b5b266d928c07150f17f1333b88e
arunmudhirajcoding <duruguarun@gmail.com>
2026-06-27 13:49:41 +0530

    renamed imprt

--- dashboard/src/components/VerticalGraph.jsx
+++ dashboard/src/components/VerticalGraph.jsx
@@ -9,7 +9,7 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { chartOptions } from "./ChartOptions";
+import { chartOptions } from "./chartOptions";
 
 ChartJS.register(
   CategoryScale,
