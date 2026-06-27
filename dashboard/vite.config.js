e53e289c052d66f3266691e8d0f0e92bd6a53c6e
arunmudhirajcoding <duruguarun@gmail.com>
2026-06-27 13:26:57 +0530

    local api configuration

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -5,8 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api': 'http://localhost:3002'
-    }
+      "/api": {
+        target: "http://localhost:3002",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
 })
